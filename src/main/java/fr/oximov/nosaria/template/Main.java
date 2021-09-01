56b88b57f0c86fc5a24f0950bc8f4e6fc12820f4
Oximov <oxifac@gmail.com>
2021-09-01 12:11:01 +0200

    lot of fix

---
+++ src/main/java/fr/oximov/nosaria/template/Main.java
@@ -0,0 +1,19 @@
+package fr.oximov.nosaria.lobby;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+
+  public static String prefix = "§3§lNosa§f§lria §7» ";
+
+  //« »
+
+  @Override
+  public void onEnable() {
+  }
+}
